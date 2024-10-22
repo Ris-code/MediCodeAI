ec8e3fcba2bc5837300d57b918d1ef4a0a612da2
Rishav Aich <aich.1@iitj.ac.in>
2024-10-22 15:35:27 +0530

    added the frontend url to cors

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ const app = express()
 const PORT = process.env.PORT || 5000; 
 
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: 'https://medicodeai.vercel.app',
     methods: ['GET', 'POST'],        // Specify allowed methods
     credentials: true 
 }));
